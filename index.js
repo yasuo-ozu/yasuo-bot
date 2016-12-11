550bc0bcaac85f6a00ca20e52c5996509d537a73
yozu <yasuo@ozu.email>
2016-12-11 17:47:26 +0900

    Changed index.js

--- index.js
+++ index.js
@@ -1,101 +1,65 @@
-// const http = require('http');
-// var port = process.env.PORT || 5000;
-// http.createServer((req, res) => {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hello World\n');
-// }).listen(port);
-// console.log('Server running at http://localhost:' + port + "/");
-//
+'use strict'
+const line = require('./index')
+const express = require('express')
+const bodyParser = require('body-parser')
+const lineClient = line.client
+const lineValidator = line.validator
+const app = express()
+app.use(bodyParser.json({
+  verify (req, res, buf) {
+    req.rawBody = buf
+  }
+}))
 
-
-
-var express = require('express');
-var request = require('request');
-var bodyParser = require('body-parser');
-var CryptoJS = require("crypto-js");
-// var config = require('./config/beta/configurations.json');
-
-var http = express();
-var port = process.env.PORT || 5000;
-// var port = config.port;
-var config = {
+// init with auth
+line.init({
   accessToken: 'OqndqEonw+rNnlCwR3wc8/TImaZ3QzTYvyKqH6mkq8OIi+ex2sAuZQ9VEzXuwieESUOaw5d3Af1HySAV6FpVrPNMj/8fM8ojDIIwnK1vZzoRMYIMRc3o0kLFSo1TdFlKxW/yCQpP22y+l+0cZJDYPAdB04t89/1O/w1cDnyilFU=',
   // (Optional) for webhook signature validation
   channelSecret: 'deaece338ebcadc7df81aa2b77cff286'
-};
+})
 
-http.use(bodyParser.json());
+/**
+ * response example (https://devdocs.line.me/ja/#webhook):
+ * {
+ *   "events": [
+ *     {
+ *       "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+ *       "type": "message",
+ *       "timestamp": 1462629479859,
+ *       "source": {
+ *         "type": "user",
+ *         "userId": "u206d25c2ea6bd87c17655609a1c37cb8"
+ *       },
+ *       "message": {
+ *         "id": "325708",
+ *         "type": "text",
+ *         "text": "Hello, world"
+ *       }
+ *     }
+ *   ]
+ * }
+ */
 
-function verifyRequest(req, res, next) {
-	// Refer to https://developers.line.me/businessconnect/development-bot-server#signature_validation
-}
+app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
+  // get content from request body
+  const promises = req.body.events.map(event => {
+    // reply message
+    return line.client
+      .replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: event.message.text
+          }
+        ]
+      })
+  })
+  Promise
+    .all(promises)
+    .then(() => res.json({success: true}))
+})
 
-http.post('/events', function(req, res) {
-	var channelSignature = req.get('x-line-signature');
-	var sha256 = CryptoJS.HmacSHA256(JSON.stringify(req.body), config.channelSecret);
-	var base64encoded = CryptoJS.enc.Base64.stringify(sha256);
-	//console.log(base64encoded + "\n\n\n" + channelSignature + "\n\n");
-	//console.log(req.headers);
-	if (base64encoded === channelSignature) {
-		var result = req.body.result;
-		if (!result || !result.length || !result[0].content) {
-			console.log("body : " + req.body);
-			res.status(471).end();
-			return;
-		}
-		res.status(200).end();
-
-		// One request may have serveral contents in an array.
-		var content = result[0].content;
-		// mid
-		var from = content.from;
-		// Content type would be possibly text/image/video/audio/gps/sticker/contact.
-		var type = content.type;
-		// assume it's text type here.
-		var text = content.text;
-
-		// Refer to https://developers.line.me/businessconnect/api-reference#sending_message
-		sendMsg(config.echoBotMid, {
-			contentType: 1,
-			toType: 1,
-			text: 'respond'
-		}, function(err) {
-			if (err) {
-				// sending message failed
-				return;
-			}
-			// message sent
-		});
-	} else {
-		res.status(472).end();
-	}
-});
-
-function sendMsg(who, content, callback) {
-	var data = {
-		to: [who],
-		toChannel: config.eventToChannelId,
-		eventType: config.eventType,
-		content: content
-	};
-
-	request({
-		method: 'POST',
-		// https://api.line.me
-		url: config.channelUrl + '/v1/events',
-		headers: {
-			'Content-Type': 'application/json',
-			'X-Line-ChannelToken': config.channelToken
-		},
-		json: data
-	}, function(err, res, body) {
-		if (err) {
-			callback(err);
-		} else {
-			callback();
-		}
-	});
-}
-
-http.listen(port);
-
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Example app listening on port 3000!')
+})
